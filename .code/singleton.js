b509a1adc2532bab289ac1640d32e5fa843e5d6e
WangYeWei <826036140@qq.com>
2022-11-10 18:02:16 +0800

    private：singleton --zh

---
+++ .code/singleton.js
@@ -0,0 +1,35 @@
+// class Singleton {
+//   constructor(name) {
+//     this.name = name
+//     this.instance = null
+//   }
+
+//   static getName() {
+//     return this.name
+//   }
+
+//   static getInstance(name) {
+
+//     !this.instance && (this.instance = new Singleton(name))
+//     return this.instance
+//   }
+// }
+
+// const a = Singleton.getInstance('wangyewei')
+// const b = Singleton.getInstance('limenghan')
+
+// console.log(a === b)
+
+// class CreateDiv {
+//   instance
+//   constructor(html) {
+//     if(this.instance) return this.instance
+//     this.html = html
+    
+//     return this.instance = this
+//   }
+
+//   init() {
+//     const div = document.
+//   }
+// }
